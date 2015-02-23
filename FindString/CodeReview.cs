ec1e728b0d240a89e7cdafc03d4b85ad61345da3
nikhiljs <humpudumpu@gmail.com>
2015-02-23 12:53:56 -0500

    Add new parameters to the file objects

--- FindString/CodeReview.cs
+++ FindString/CodeReview.cs
@@ -23,13 +23,13 @@ namespace FindString
 
 		public void GetIncident(int incident)
 		{
-			fileList.Add(new FileObject("filename1", "devBranch1", "comments1"));
-			fileList.Add(new FileObject("filename2", "devBranch2", "comments2"));
-			fileList.Add(new FileObject("filename3", "devBranch3", "comments3"));
-			fileList.Add(new FileObject("filename4", "devBranch4", "comments4"));
-			fileList.Add(new FileObject("filename5", "devBranch5", "comments5"));
-			fileList.Add(new FileObject("filename6", "devBranch6", "comments6"));
-			fileList.Add(new FileObject("filename6", "devBranch7", "comments7"));
+			fileList.Add(new FileObject("filename1", "devBranch1", "comments1", 10, 16, "Author1"));
+			fileList.Add(new FileObject("filename2", "devBranch2", "comments2", 20, 26, "Author2"));
+			fileList.Add(new FileObject("filename3", "devBranch3", "comments3", 30, 36, "Author3"));
+			fileList.Add(new FileObject("filename4", "devBranch4", "comments4", 40, 46, "Author4"));
+			fileList.Add(new FileObject("filename5", "devBranch5", "comments5", 50, 56, "Author5"));
+			fileList.Add(new FileObject("filename6", "devBranch6", "comments6", 60, 66, "Author6"));
+			fileList.Add(new FileObject("filename6", "devBranch7", "comments7", 70, 76, "Author7"));
 			if (OnFileListUpdate == null)
 				return;
 			FileListUpdateArgs args = new FileListUpdateArgs(fileList);
--- FindString/FileObject.cs
+++ FindString/FileObject.cs
@@ -11,16 +11,22 @@ namespace FindString
 		public string DevBranch { get; set; }
 		public string Filename { get; set; }
 		public string Comments { get; set; }
+		public int CheckOutChangeSet { get; set; }
+		public int CheckInChangeSet { get; set; }
+		public string Author { get; set; }
 
 		public FileObject() 
 		{ }
 
 		//Contructor used to test the observable collection
-		public FileObject(string filename, string devBranch, string comments)
+		public FileObject(string filename, string devBranch, string comments, int checkoutNo, int checkinNo, string author)
 		{
 			this.DevBranch = devBranch;
 			this.Filename = filename;
 			this.Comments = comments;
+			this.CheckOutChangeSet = checkoutNo;
+			this.CheckInChangeSet = checkinNo;
+			this.Author = author;
 		}
 	}
 }
