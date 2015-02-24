eaf8fa9e9e157973e10c7cc1383226eaae0158ff
nikhiljs <humpudumpu@gmail.com>
2015-02-24 11:22:32 -0500

    Remove test code. Make the incidentNo parameter unsigned int

--- FindString/CodeReview.cs
+++ FindString/CodeReview.cs
@@ -24,19 +24,8 @@ namespace CodeReview
 			tf = new TFUtility();
 		}
 
-		public void GetIncident(int incident)
+		public void GetIncident(uint incident)
 		{
-			//Test code
-			//fileList.Add(new FileObject("filename1", "devBranch1", "comments1", 10, 16, "Author1"));
-			//fileList.Add(new FileObject("filename2", "devBranch2", "comments2", 20, 26, "Author2"));
-			//fileList.Add(new FileObject("filename3", "devBranch3", "comments3", 30, 36, "Author3"));
-			//fileList.Add(new FileObject("filename4", "devBranch4", "comments4", 40, 46, "Author4"));
-			//fileList.Add(new FileObject("filename5", "devBranch5", "comments5", 50, 56, "Author5"));
-			//fileList.Add(new FileObject("filename6", "devBranch6", "comments6", 60, 66, "Author6"));
-			//fileList.Add(new FileObject("filename6", "devBranch7", "comments7", 70, 76, "Author7"));
-
-			tf.GetChangesFileList(incident);
-
 			PopulateFileObjects(tf.GetAssociations(incident));
 			if (OnFileListUpdate == null)
 				return;
