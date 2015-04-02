68341e2f02d4c9b290dc46ab2f26af2fea94b61c
nikhiljs <humpudumpu@gmail.com>
2015-04-02 15:35:58 -0400

    Add a new page - ReviewNotes

---
+++ CodeReview/ReviewNotes.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CodeReview
+{
+	/// <summary>
+	/// Interaction logic for ReviewNotes.xaml
+	/// </summary>
+	public partial class ReviewNotes : Window
+	{
+		private ReviewNotesViewModel viewModel;
+
+		public ReviewNotes(ReviewNotesViewModel viewModel)
+		{
+			InitializeComponent();
+			this.viewModel = viewModel;
+		}
+	}
+}
